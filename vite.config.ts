02160594a32775f8eaed893ddad6ce562784f35c
Andy Nugrahan Putra <andybima6@gmail.com>
2025-11-11 12:34:07 +0700

    fix: Update server configuration in vite.config.ts for strict port and HMR settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/erp/',  // HARUS ADA INI
   server: {
     host: "::",
     port: 8080,
+    strictPort: true,
+    hmr: {
+      clientPort: 443,
+      host: 'max.biz.id',
+      protocol: 'wss',
+    },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
